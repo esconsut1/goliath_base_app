f3a583e683a790119add8853c2e15e581d1fab85
Ericson Smith <esconsult1@gmail.com>
2011-12-21 15:23:22 -0500

    Added some stub assets

--- main.rb
+++ main.rb
@@ -4,7 +4,20 @@ require './config/startup'
 #- This gets called and instantiated
 class Main < Goliath::API
 
-	#- Default routes
+	#- Load static assets. Remove when your app is behind a proxy
+	#  let the proxy do the dirty work
+	use(Rack::Static,
+		:root => Goliath::Application.app_path("public"),
+		:urls => ["/favicon.ico", '/stylesheets', '/javascripts', '/images'])
+	)
+
+	#- Some other routes to your custom Goliath class
+	# For more routing examples, see:
+	# https://github.com/postrank-labs/goliath/blob/master/examples/rack_routes.rb 
+
+	# map '/fun/:id', MyClass
+
+	#- Default route to automate controllers
 	map '/:controller/(/:action(/:id))', Dispatch
 
 	def response(env)
